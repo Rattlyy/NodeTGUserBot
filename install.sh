f9b8c97ba67696af1091191ef7e6e6d0ad4782be
zRattly <40183121+zRattly@users.noreply.github.com>
2019-05-31 17:55:57 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "[NodeTGUserBot] - Installer"
+echo "[NodeTGUserBot] - Installing Required Packages"
+apt-get update
+apt-get install nodejs npm git screen gcc libreadline-dev libconfig-dev libpng-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev make  -y
+echo "[NodeTGUserBot] - Packages Installed"
+echo "[NodeTGUserBot] - Installing Telegram-CLI"
+git clone --recursive https://github.com/vysheng/tg.git && cd tg
+./configure
+echo "[NodeTGUserBot] - Compiling Telegram-CLI"
+make
+echo "[NodeTGUserBot] - Compiled Telegram-CLI"
+echo "[NodeTGUserBot] - Installing Required Node.js Dependencies"
+npm i --save tg-cli-node
+npm i -g pm2
+echo "[NodeTGUserBot] - Installed Node.js Dependencies"
+echo "[NodeTGUserBot] - Install Finished"
+echo "[NodeTGUserBot] - To run your code, use pm2 start index.js"
