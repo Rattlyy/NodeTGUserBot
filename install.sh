f51faf6fba2f10e0c3c6de05878327653515134a
Rattly <40183121+zRattly@users.noreply.github.com>
2019-06-01 09:32:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo "[NodeTGUserBot] - Installer"
 echo "[NodeTGUserBot] - Installing Required Packages"
 sudo apt-get update
 sudo apt-get install nodejs npm -y
-sudo apt-get install git screen gcc libreadline-dev libconfig-dev libpng-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev make  -y
+sudo apt-get install git screen gcc libssl1.0-dev libreadline-dev libconfig-dev libpng-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev make  -y
 echo "[NodeTGUserBot] - Packages Installed"
 echo "[NodeTGUserBot] - Installing Telegram-CLI"
 git clone --recursive https://github.com/vysheng/tg.git && cd tg
